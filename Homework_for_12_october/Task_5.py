39f0bdc93c11bc74333a737fe7fd11df7c5f53a9
MK <makhov.ks@phystech.edu>
2025-10-09 00:37:13 +0300

    first commit

--- Homework_for_12_october/Task_5.py
+++ Homework_for_12_october/Task_5.py
@@ -3,8 +3,14 @@ import numpy as np
 import pandas as pd
 df = pd.read_csv("C:/Users/user/Downloads/BTC_data.csv")
 x = df['time']
+x_p = x[::25]
 y = df['close']
 plt.plot(x,y)
+print(x)
+print(x_p)
+plt.xticks(x_p, rotation=90)
+plt.subplots_adjust(bottom=0.27)
+plt.grid(True)
 plt.show()
 plt.savefig('Task_5_graph.png', dpi=500)
 print(df)
